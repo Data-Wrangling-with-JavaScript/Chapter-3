d5f85c7d2c515dbbbea0d149bfbdc6bc1cb8dd4b
Evan Staton <1477856+sestaton@users.noreply.github.com>
2019-01-25 14:23:58 -0800

    Update listing-3.12.js
    Limit search results to the API-defined max number (20k) so we avoid 400 error

--- listing-3.12.js
+++ listing-3.12.js
@@ -2,7 +2,7 @@
 
 const importCsvFromRestApi = require('./toolkit/importCsvFromRestApi.js');
 
-const url = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=2017-01-01&endtime=2017-03-02";
+const url = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=2017-01-01&endtime=2017-03-02&limit=20000";
 importCsvFromRestApi(url)
     .then(data => {
         console.log(data);
